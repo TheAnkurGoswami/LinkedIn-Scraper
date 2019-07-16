73b84ec4405673b2af86384865516a2f6c9c945b
अंkur गोswami <kinglusifer1401@gmail.com>
2019-07-16 15:25:06 +0530

    Update main.py

--- main.py
+++ main.py
@@ -13,3 +13,10 @@ driver=webdriver.Chrome("./driver/chromedriver.exe")
 driver.get('https://www.linkedin.com/uas/login')
 
 driver=login(driver)
+
+page_link='https://www.linkedin.com/mynetwork/invite-connect/connections/'
+page_users_links=get_page_users_links(driver,page_link)
+
+user_data=get_page_users_data(page_users_links)
+
+print(user_data)

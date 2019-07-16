a099f9c8936a8f0cbc8ef76d1daef4bbad1c209e
अंkur गोswami <kinglusifer1401@gmail.com>
2019-07-17 00:06:45 +0530

    Update main.py

--- main.py
+++ main.py
@@ -24,10 +24,10 @@ driver.get('https://www.linkedin.com/uas/login')
 
 driver=login(driver)
 page_link='https://www.linkedin.com/mynetwork/invite-connect/connections/'
+
 page_users_links=get_page_users_links(driver,page_link)
-
-len(page_users_links)
-
 user_data=get_page_users_data(driver,page_users_links)
+
+pd.Dataframe(user_data)
 
 driver.quit()

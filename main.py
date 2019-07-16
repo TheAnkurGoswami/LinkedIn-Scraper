10cf4dddfb85b90c21dcb83bf0275ec75a4347c4
अंkur गोswami <kinglusifer1401@gmail.com>
2019-07-16 16:10:33 +0530

    Update main.py

--- main.py
+++ main.py
@@ -4,19 +4,31 @@ from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 import numpy as np
 import pandas as pd
-
 import shelve
-from auth.login import login
 
-driver=webdriver.Chrome("./driver/chromedriver.exe")
+from auth.login import login
+from crawl.page_users import get_page_users_links
+from crawl.page_users import get_page_users_data
+
+choice=input('Keep Working Hidden? (y/n)\n').lower()
+if choice=='y':
+    opt=webdriver.ChromeOptions()
+    opt.add_argument('headless')
+
+elif choice=='n':
+    opt=None
+     
+    
+driver=webdriver.Chrome("./driver/chromedriver.exe",chrome_options=opt)
 
 driver.get('https://www.linkedin.com/uas/login')
 
 driver=login(driver)
-
 page_link='https://www.linkedin.com/mynetwork/invite-connect/connections/'
 page_users_links=get_page_users_links(driver,page_link)
 
-user_data=get_page_users_data(page_users_links)
+len(page_users_links)
 
-print(user_data)
+user_data=get_page_users_data(driver,page_users_links)
+
+driver.quit()
